f0ef1c7a5a680dd7233c8891ea07fa5a460320b3
Ari <acas_2014@outlook.es>
2025-06-19 01:03:55 -0500

    feature get user by id

--- src/main/java/com/wiwitech/mecanetbackend/iam/interfaces/rest/resources/UserResource.java
+++ src/main/java/com/wiwitech/mecanetbackend/iam/interfaces/rest/resources/UserResource.java
@@ -2,5 +2,5 @@ package com.wiwitech.mecanetbackend.iam.interfaces.rest.resources;
 
 import java.util.List;
 
-public record UserResource(Long id, String username, List<String> roles) {
+public record UserResource(Long id, String username, String name, String email, List<String> roles) {
 }
